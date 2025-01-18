486c5ea28337d140a9334a23939b490568d8d0ec
EmreAtesROC <2191023@talnet.nl>
2025-01-18 19:02:23 +0100

    Add files via upload

---
+++ web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/planeten', function () {
+    return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"]
+});
